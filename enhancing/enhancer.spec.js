e3c041c689919203a453c53e4ca31af4466a6538
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-14 21:13:49 -0400

    Did first test.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { succeed, fail, repair, get } = require("./enhancer.js");
+
+describe("enhance unit test", () => {
+  describe("repair", () => {
+    it("durability restored to 100", () => {
+      const item = {
+        name: "Computer",
+        durability: 20,
+        enhancement: 1,
+      };
+      const repairedItem = repair(item);
+      expect(repairedItem.durability).toBe(100);
+    });
+  });
+});
